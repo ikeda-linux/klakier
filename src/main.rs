8104062cb06729e599fb4debe090b60a1d1f569a
michal <michal@tar.black>
2021-12-11 03:43:21 +0000

    i forgot what i changed here tbh

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::env;
 use std::fs;
 use std::io::Write;
 use std::path::Path;
+use semver::Version;
 use libdlta::base::{install::install, uninstall::uninstall};
+use libdlta::base::structs::Package;
 use libdlta::database::{query::query, search::search};
 use libdlta::advanced::freeze::freeze;
 
@@ -46,36 +48,142 @@ fn main() {
 
     match oper.as_str() {
         "install" => {
+            let mut packages_nodeps: Vec<Package> = Vec::new();
+            let mut packages_deps: Vec<Package> = Vec::new();
+            let mut not_found: Vec<String> = Vec::new();
             for package in packages.iter() {
                 for repo in &repos.local {
-                    let pathstr = format!("{}/{}.tar.zst", repo.location, package);
-                    let path = Path::new(&pathstr);
-                    if path.exists() {
-                     std::fs::copy(&pathstr, format!("/tmp/libdlta/pkg/{}", package)).unwrap_or_else(|_| {
-                            eprintln!("Could not copy {} to /tmp/libdlta/pkg/{}", pathstr, package);
-                            std::process::exit(1);
-                        });
-                    } else {
-                        eprintln!("Could not find {} in local repos", package);
-                        eprintln!("{} {}", pathstr, path.exists());
+                    // breaks if package already found
+                    if Path::new(&format!("/tmp/libdlta/pkg/{}.tar.zst", package)).exists() {
+                        break;
                     }
-                }
-                for repo in &repos.remote {
-                    for url in &repo.locations {
-                        let path = format!("{}/{}.tar.zst", url, package);
-                        let resp = reqwest::blocking::get(&path).unwrap();
-                        if resp.status().is_success() {
-                            let mut file = fs::File::create(format!("/tmp/libdlta/pkg/{}", package)).unwrap();
-                            file.write_all(&resp.bytes().unwrap()).unwrap();
-                            break;
+                    // downloads the remote repository db and queries it for the package
+                    let db_pathstr = format!("/tmp/dlta/repo_dbs/{}/db.sqlite", repo.name);
+                    fs::create_dir_all(format!("/tmp/dlta/repo_dbs/{}", repo.name)).unwrap_or_else(|err| {
+                        eprintln!("Could not create database directory: {}", err);
+                        std::process::exit(1);
+                    });
+                    let db_path = Path::new(&db_pathstr);
+                    let db_loc = format!("{}/{}", repo.location, "db.sqlite");
+                    let mut db_file = fs::File::create(&db_path).unwrap_or_else(|err| {
+                        eprintln!("Could not create database file: {}", err);
+                        std::process::exit(1);
+                    });
+                    let db_data = fs::read(&db_loc).unwrap_or_else(|err| {
+                        eprintln!("Could not read database file: {}", err);
+                        std::process::exit(1);
+                    });
+                    db_file.write_all(&db_data).unwrap_or_else(|err| {
+                        eprintln!("Could not write database file: {}", err);
+                        std::process::exit(1);
+                    });
+                    let res = query(&package, &db_path);
+                    let re2 = query(&package, &db_path); // lousy way to do this
+                    let re3 = query(&package, &db_path); // but it works
+
+                    // pushes the package to not_found if it's not found in the repo
+                    if re2.name == "" {
+                        not_found.push(package.to_string());
+                    } else {
+                        packages_nodeps.push(re2);
+                    }
+                    
+                    // recursively queries the database for the package's dependencies and pushes them to packages_deps until no more unique dependencies are found
+                    let deps = re3.dependencies.clone().unwrap_or(vec![]);
+                    for dep in deps.iter() {
+                        let res = query(&dep, &db_path);
+                        if res.name != "" {
+                            let mut found = false;
+                            for p in packages_deps.iter() {
+                                if p.name == res.name {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if !found {
+                                packages_deps.push(res);
+                            }
+                        }
+                    }
+                    
+                    // queries the repo for all of the package's dependencies and if any return "", push them to not_found
+                    for dep in deps.iter() {
+                        let res = query(&dep, &db_path);
+                        if res.name == "" {
+                            not_found.push(dep.to_string());
                         }
                     }
                 }
-                install(Path::new(&format!("/tmp/libdlta/pkg/{}", package))).unwrap_or_else( |err| {
-                    eprintln!("Problem installing {}: {}", package, err);
-                    std::process::exit(1);
-                });
+                for repo in &repos.remote {
+                    // breaks if package already found
+                    if Path::new(&format!("/tmp/libdlta/pkg/{}.tar.zst", package)).exists() {
+                        break;
+                    }
+                    // downloads the remote repository db and queries it for the package
+                    let db_pathstr = format!("/tmp/dlta/repo_dbs/{}/db.sqlite", repo.name);
+                    let db_path = Path::new(&db_pathstr);
+                    let db_url = format!("{}/{}", repo.locations[0], "db.sqlite");
+                    let mut db_file = fs::File::create(&db_path).unwrap_or_else(|err| {
+                        eprintln!("Could not create database file: {}", err);
+                        std::process::exit(1);
+                    });
+                    let mut db_response = reqwest::blocking::get(&db_url).unwrap_or_else(|err| {
+                        eprintln!("Could not download database file: {}", err);
+                        std::process::exit(1);
+                    });
+                    db_response.copy_to(&mut db_file).unwrap_or_else(|err| {
+                        eprintln!("Could not copy database file: {}", err);
+                        std::process::exit(1);
+                    });
+                    let res = query(&package, &db_path);
+                    let re2 = query(&package, &db_path); // lousy way to do this
+                    let re3 = query(&package, &db_path); // but it works
+
+                    // only pushes the package to packages_nodeps if it's not already in packages_nodeps and if it already is, only if the parsed semver is higher
+                    if res.name != "" {
+                        let mut found = false;
+                        for p in packages_nodeps.iter() {
+                            if p.name == res.name {
+                                if Version::parse(&p.version).unwrap() < Version::parse(&res.version).unwrap() {
+                                    packages_nodeps.retain(|x| x.name != res.name);
+                                    packages_nodeps.push(res);
+                                }
+                                found = true;
+                                break;
+                            }
+                        }
+                        if found {
+                            packages_nodeps.push(re2);
+                        } else {
+                            not_found.push(package.to_string());
+                        }
+                    }
+
+                    // recursively queries the database for the package's dependencies and pushes them to packages_deps until no more unique dependencies are found
+                    let deps = re3.dependencies.clone().unwrap_or(vec![]);
+                    for dep in deps.iter() {
+                        let dep_res = query(&dep, &db_path);
+                        if dep_res.name != "" {
+                            let mut found = false;
+                            for p in packages_deps.iter() {
+                                if p.name == dep_res.name {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if found {
+                                packages_deps.push(dep_res);
+                            } else {
+                                panic!("Package {} requested dependency {} but it was not found", package, dep);
+                            }
+                        }
+                    }
+                }
             }
+            // DEBUG: prints out all vecs in raw format for testing
+            println!("expl: {:?}\n", packages_nodeps);
+            println!("deps: {:?}\n", packages_deps);
+            println!("ntfd: {:?}", not_found);
         }
         "uninstall" => {
             for package in packages.iter() {
@@ -85,7 +193,7 @@ fn main() {
         "query" => {
             let mut nonmatches: Vec<String> = Vec::new();
             for arg in packages {
-                let pkg = query(&arg);
+                let pkg = query(&arg, Path::new(libdlta::database::initialise::DATABASE_PATH));
                 if pkg.name == "" {
                     nonmatches.push(arg.to_string());
                 } else {
@@ -104,7 +212,7 @@ fn main() {
         "search" => {
             let mut nonmatches: Vec<String> = Vec::new();
             for arg in packages {
-                let pkgs = search(&arg);
+                let pkgs = search(&arg, Path::new(libdlta::database::initialise::DATABASE_PATH));
                 for pkg in pkgs {
                     if pkg.name == "" {
                         nonmatches.push(arg.to_string());
